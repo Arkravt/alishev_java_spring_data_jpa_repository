4d4721de80aa320fd9a7c13ef3fa76a0e88e5e6b
Kravtsev Artem <arkravt@gmail.com>
2024-08-02 09:14:46 +0400

    02.08.2024

---
+++ src/main/java/ru/spring/datajpa/dao/PersonDAO.java
@@ -0,0 +1,63 @@
+package ru.spring.datajpa.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.spring.datajpa.models.Person;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Person> index() {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("FROM Person", Person.class).getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public Person show(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.get(Person.class, id);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.save(person);
+    }
+
+    @Transactional
+    public void update(int id,Person updatedPerson) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.update(updatedPerson);
+//        OR
+//        Person personToBeUpdated = session.get(Person.class, id);
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
+//        personToBeUpdated.setAddress(updatedPerson.getAddress());
+    }
+
+    @Transactional
+    public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Person person = session.get(Person.class, id);
+        session.remove(person);
+    }
+}
---
+++ src/main/java/ru/spring/datajpa/models/Person.java
@@ -0,0 +1,86 @@
+package ru.spring.datajpa.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
+    private String name;
+
+    @Min(value = 0, message = "Age should be greater than 0")
+    @Column(name = "age")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    @Column(name = "email")
+    private String email;
+
+    @NotEmpty(message = "Address should not be empty")
+    @Column(name = "address")
+    private String address;
+
+    public Person() {
+
+    }
+
+    public Person(String name, int age, String email, String address) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
